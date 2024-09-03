9aea41d92392ff0a349965eda09d6938026c5013
unknown <sachin.danayak@gmail.com>
2024-09-03 23:29:23 +0530

    updated spring boot libs to run service

--- billingService/billingService/src/main/java/co/in/sample/billingService/BillingController.java
+++ billingService/billingService/src/main/java/co/in/sample/billingService/BillingController.java
@@ -3,18 +3,19 @@ package co.in.sample.billingService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController("/api")
 public class BillingController {
 
-    @PostMapping("/api/billing/")
+    @PostMapping(path = "/billing")
     public ResponseEntity<String> billingRequested(@RequestBody BillingData billing) {
-        if(billing != null && "errorProduct".equalsIgnoreCase(billing.getProductName())) {
+        if(billing != null && billing.getProductName() != null && StringUtils.pathEquals("errorProduct", billing.getProductName())) {
             return ResponseEntity.internalServerError().body("Failed to bill");
         }
         return ResponseEntity.ok("Billing Completed");
-
     }
 }
--- billingService/billingService/src/main/java/co/in/sample/billingService/BillingServiceApplication.java
+++ billingService/billingService/src/main/java/co/in/sample/billingService/BillingServiceApplication.java
@@ -1,9 +1,13 @@
 package co.in.sample.billingService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@AutoConfiguration
 public class BillingServiceApplication {
 
 	public static void main(String[] args) {
