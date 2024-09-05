e557aecd2526412af210ac7be5de6e60912c5fae
unknown <sachin.danayak@gmail.com>
2024-09-05 13:31:55 +0530

    included api to dispatch

--- billingService/billingService/src/main/java/co/in/sample/billingService/BillingController.java
+++ billingService/billingService/src/main/java/co/in/sample/billingService/BillingController.java
@@ -1,8 +1,8 @@
 package co.in.sample.billingService;
 
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/api")
 public class BillingController {
 
-    @PostMapping(path = "/billing")
+    @PostMapping(path = "/billing", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> billingRequested(@RequestBody BillingData billing) {
         if(billing != null && billing.getProductName() != null && StringUtils.pathEquals("errorProduct", billing.getProductName())) {
             return ResponseEntity.internalServerError().body("Failed to bill");
