eb1d8f86624f409d3d35427efd11ffe27e4135ae
unknown <sachin.danayak@gmail.com>
2024-09-11 14:52:21 +0530

    added order call in orchestration and cleanup

--- orderService/orderService/src/main/java/co/in/sample/orderService/OrderController.java
+++ orderService/orderService/src/main/java/co/in/sample/orderService/OrderController.java
@@ -1,5 +1,7 @@
 package co.in.sample.orderService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
@@ -9,14 +11,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController("/api")
+@RestController
 public class OrderController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
     @PostMapping(path = "/order", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> orderRequest(@RequestBody OrderDetails orderDetails) {
+        if (ObjectUtils.isEmpty(orderDetails) || ObjectUtils.isEmpty(orderDetails.getOrderId())) {
+            LOGGER.error("Found request or order Id blank in request");
+            return ResponseEntity.internalServerError().body("Failed to Order for error product");
+        }
         if (!ObjectUtils.isEmpty(orderDetails) && StringUtils.hasLength(orderDetails.getOrderDescription())
                 && StringUtils.pathEquals("errorProduct", orderDetails.getOrderDescription())) {
-            return ResponseEntity.internalServerError().body("Failed to Order for error product");
+            LOGGER.error("Failed due to invalid product order request");
+            return ResponseEntity.internalServerError().body("Failed to Order for invalid product");
         }
         return ResponseEntity.ok("Order Completed");
     }
